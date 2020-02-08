45a680da09ab171e9697181e711640ee191ba7c8
Dania King <dania@fortunebuilders.com>
2020-02-08 10:05:04 -0800

    Finally Works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -31,7 +31,7 @@ var Guess = [
 // Tracking Variables
 var Wins = 0;
 var Losses = 0;
-var GuessLeft = 20;
+var GuessLeft = 10;
 var UserInput = [];
 var pcchoice;
 
@@ -40,31 +40,39 @@ document.onkeyup = function(event) {
   var keystroke = event.key;
   //   make keystroke lowercase
   keystroke = keystroke.toLowerCase();
+  // Add key to input list
   updateChoice(keystroke);
+  // Check to see if the choice matches the PC choice
   checkYaChoice(keystroke);
+  // log the userchoice to the console
   console.log(keystroke);
 };
 
-// Update User Choice
+//
 function updateChoice(input) {
+  // add the user choice to input array
   UserInput.push(input);
   console.log(input);
+  // update the dialogue to the user
   document.getElementById("guess").innerHTML =
     "Your guesses so far: " + UserInput;
 }
 
-// Update Front End HTML
+// Compare userchoice to PC Choice
 function checkYaChoice(input) {
   if (input === pcchoice) {
+    // If match update wins and reset
     Wins = Wins + 1;
     document.getElementById("w").innerHTML = "Wins: " + Wins;
     reset();
   } else {
+    // if didn't match subtract from gueses left until 0, add to losses, and reset
     GuessLeft = GuessLeft - 1;
     if (GuessLeft === 0) {
       Losses = Losses + 1;
       reset();
     }
+    // Update counts on the page
     document.getElementById("l").innerHTML = "Losses: " + Losses;
     document.getElementById("left").innerHTML = "Guesses Left:" + GuessLeft;
   }
